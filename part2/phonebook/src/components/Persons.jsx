1942d93d758f1ae493bdbe7e64862ef7b05c97df
honda <clio209@gmail.com>
2023-12-12 16:05:17 +0900

    2.13 モジュールの追加

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
 const Persons = ({ persons, filterName }) => {
   const filteredPersons = persons.filter(person => person.name.includes(filterName))
   return (
@@ -7,4 +10,20 @@ const Persons = ({ persons, filterName }) => {
   );
 }
 
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
 export default Persons;
+export { getAll, create, update};
